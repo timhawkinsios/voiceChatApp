068b1f7adba78367a95c166b71490b72708221a5
timhawkinsios <timothyhaydenhawkinsios@gmail.com>
2018-09-03 20:27:40 +0100

    removes storyboard and sets initial vc

--- voiceChatApp/ViewController.swift
+++ voiceChatApp/ViewController.swift
@@ -8,18 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class homeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.red
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
