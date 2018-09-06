0c5315bcf2dc887ce6548ac6d6caa59056c2bc8a
timhawkinsios <timothyhaydenhawkinsios@gmail.com>
2018-09-06 11:24:44 +0100

    adds tests to confirm views have been added correctly

--- voiceChatAppTests/home/homeViewTests.swift
+++ voiceChatAppTests/home/homeViewTests.swift
@@ -9,6 +9,14 @@
 import XCTest
 @testable import voiceChatApp
 
+extension homeViewController {
+    
+    func startViewLifecycle() {
+        view.setNeedsLayout()
+        view.layoutIfNeeded()
+    }
+}
+
 class homeViewTests: XCTestCase {
     
     var controller: homeViewController!
@@ -16,6 +24,7 @@ class homeViewTests: XCTestCase {
     override func setUp() {
         super.setUp()
         controller = homeViewController()
+        controller.startViewLifecycle()
     }
     
     override func tearDown() {
@@ -25,25 +34,30 @@ class homeViewTests: XCTestCase {
     func testAddDashboardRendersDashboard() {
         controller.addDashboardView()
         XCTAssertNotNil(controller.dashboardView)
+        XCTAssertNotNil(controller.dashboardView.superview, "dashboardView not added to homeViewController")
     }
     
     func testAddContactButtonIsRendered() {
         controller.addDashboardView()
         XCTAssertNotNil(controller.addContactButton)
+        XCTAssertNotNil(controller.addContactButton.superview, "addContactButton not added to dashboardView")
     }
     
     func testTableOptionViewIsRendered() {
         controller.addTableOptionView()
         XCTAssertNotNil(controller.tableOptionView)
+        XCTAssertNotNil(controller.tableOptionView.superview, "tableOptionView not added to homeViewController")
     }
     
     func testContactsButtonIsRendered() {
         controller.addTableOptionView()
         XCTAssertNotNil(controller.contactsButton)
+        XCTAssertNotNil(controller.contactsButton.superview, "contactsButton not added to homeViewController")
     }
     
     func testRecentsButtonIsRendered() {
         controller.addTableOptionView()
         XCTAssertNotNil(controller.recentsButton)
+        XCTAssertNotNil(controller.recentsButton.superview, "recentsButton not added to homeViewController")
     }
 }
